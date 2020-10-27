238da0cf4bc0173a129107fecbddd3ff06c3b16d
Sky Florence <55286296+skyflocodes@users.noreply.github.com>
2020-10-26 22:49:13 -0400

    Updated style
    style update and a few visual changes

--- controllers/ChartViewController.java
+++ controllers/ChartViewController.java
@@ -13,30 +13,8 @@ import java.io.IOException;
 public class ChartViewController {
     @FXML
     private BarChart<String, Integer> userChart;
-    @FXML
-    private PieChart gPUChart;
-    @FXML
-    private PieChart cPUChart;
 
     public void changeToTableView(javafx.event.ActionEvent actionEvent) throws IOException {
         SceneChange.changeScene(actionEvent, "../views/TableView.fxml", "Port Royale Records - Table View");
-    }
-
-    public void displayUserChart(ActionEvent actionEvent) {
-        userChart.setVisible(true);
-        gPUChart.setVisible(false);
-        cPUChart.setVisible(false);
-    }
-
-    public void displayGPUChart(ActionEvent actionEvent) {
-        userChart.setVisible(false);
-        gPUChart.setVisible(true);
-        cPUChart.setVisible(false);
-    }
-
-    public void displayCPUChart(ActionEvent actionEvent) {
-        userChart.setVisible(false);
-        gPUChart.setVisible(false);
-        cPUChart.setVisible(true);
     }
 }
