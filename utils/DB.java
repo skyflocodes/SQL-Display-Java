7969f37b92f7401d1c1a1ec99bead44a7e6f013d
Sky Florence <55286296+skyflocodes@users.noreply.github.com>
2020-10-26 21:04:23 -0400

    Added Db connect Util
    util connects to database and fetches the data from local SQL server

--- utils/DB.java
+++ utils/DB.java
@@ -1,5 +1,45 @@
 package utils;
 
+import models.Participant;
+
+import java.sql.*;
+import java.util.ArrayList;
+
 public class DB {
-    private String user
+    private static String username = "root";
+    private static String password = "TheTiger-007";
+
+    public static ArrayList<Participant> getAllParticipants() throws SQLException {
+        ArrayList<Participant> participants = new ArrayList<>();
+        Connection conn = null;
+        Statement sqlStatement = null;
+        ResultSet resultSet = null;
+        try{
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/portroyale",username,password);
+            sqlStatement = conn.createStatement();
+            resultSet = sqlStatement.executeQuery("SELECT * FROM portroyale.portrecords;");
+            while (resultSet.next()){
+                Participant newParticipant = new Participant(resultSet.getInt("Place"),
+                                                                resultSet.getString("User"),
+                                                                resultSet.getInt("Score"),
+                                                                resultSet.getString("GPU"),
+                                                                resultSet.getString("CPU"));
+                    participants.add(newParticipant);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            if(conn!= null){
+                conn.close();
+            }
+            if(resultSet != null){
+                resultSet.close();
+            }
+            if (sqlStatement != null){
+                sqlStatement.close();
+            }
+            return participants;
+        }
+    }
 }
